fde09050aaaabb6321ec70876f609b1ea997d6a2
admin <admin@YD-21>
2022-03-15 09:14:46 +0900

    입출력문 숙제 오류 수정

--- HelloWorld/src/com/edu/MathExample.java
+++ HelloWorld/src/com/edu/MathExample.java
@@ -12,10 +12,12 @@ public class MathExample {
 		System.out.println("두번째 숫자를 입력해 주세요.");
 		num2 = scn.nextInt();
 		
-		System.out.println("두 수의 합 :" + num1+num2);
+		System.out.println("두 수의 합 :" + (num1+num2));
 		System.out.println("두 수의 차 :" + (num1-num2));
-		System.out.println("두 수의 곱 :" + num1*num2);
-		System.out.println("두 수의 몫 :" + num1/num2);
-		System.out.println("나눈 나머지 :" + num1%num2);
+		System.out.println("두 수의 곱 :" + (num1*num2));
+		System.out.println("두 수의 몫 :" + (num1/num2));
+		System.out.println("나눈 나머지 :" + (num1%num2));
+		
+		scn.close();
 	}
 }
