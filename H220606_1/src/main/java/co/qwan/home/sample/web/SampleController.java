69425d2003ff223ca3132eaa8132062d513c6584
qwanchoi <chlrbdhks92@gmail.com>
2022-06-08 22:01:36 +0900

    컨트롤러, 권한별 페이지

---
+++ H220606_1/src/main/java/co/qwan/home/sample/web/SampleController.java
@@ -0,0 +1,25 @@
+package co.qwan.home.sample.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/sample")
+public class SampleController {
+	
+	@GetMapping("/all")
+	public void doAll() {
+		System.out.println("do all...");
+	}
+	
+	@GetMapping("/member")
+	public void doMemeber() {
+		System.out.println("logined mamber...");
+	}
+	
+	@GetMapping("/admin")
+	public void doAdmin() {
+		System.out.println("admin only...");
+	}
+}
