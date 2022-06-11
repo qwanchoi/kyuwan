b2f8c0f89585b7725d7899f4534231de64a53416
qwanchoi <chlrbdhks92@gmail.com>
2022-06-11 10:24:15 +0900

    H0606 로그인 성공 헨들러

---
+++ H220606_1/src/main/java/co/qwan/home/security/CustomLoginSuccessHandler.java
@@ -0,0 +1,41 @@
+package co.qwan.home.security;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+public class CustomLoginSuccessHandler implements AuthenticationSuccessHandler {
+
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+			Authentication authentication) throws IOException, ServletException {
+		System.out.println("Login Success");
+		
+		List<String> roleNames = new ArrayList<>();
+		
+		authentication.getAuthorities().forEach(authority -> {
+			roleNames.add(authority.getAuthority());
+		});
+		
+		System.out.println("ROLE NAMES: "+ roleNames);
+		if (roleNames.contains("ROLE_ADMIN")) {
+			response.sendRedirect(request.getContextPath()+"/sample/admin");
+			return;
+		}
+		
+		if(roleNames.contains("ROLE_MEMBER")) {
+			response.sendRedirect(request.getContextPath()+"/sample/member");
+			return;
+		}
+		
+		response.sendRedirect(request.getContextPath()+"/");
+	}
+
+}
